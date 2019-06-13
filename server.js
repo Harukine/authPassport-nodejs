618a45c592a3cdc48a2b5b39de07effcfa7ff185
Harukine <jacobhtnguyen@gmail.com>
2019-06-13 16:38:35 -0400

    pulling the routes into server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+// Pull in our api routes
+const users = require("./routes/api/users");
 
 // Initialize our app
 const app = express();
@@ -25,6 +29,15 @@ mongoose
     )
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
